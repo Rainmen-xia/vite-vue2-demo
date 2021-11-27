d6908ffc938f6ea91dca632c219b44299f61115f
rainmenxia <rainmenxia@tencent.com>
2021-11-27 23:56:17 +0800

    add demo

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <p class="test">文字：{{count}}</p>
+    <i class="icon"></i>
+  </div>
+</template>
+<script>
+
+export default{
+  data(){
+    return {
+      count:1
+    }
+  }
+}
+</script>
+
+<style scoped src="./app.less"></style>
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
+import { resolve } from 'path';
+// import { createSvgPlugin } from "vite-plugin-vue2-svg";
+// function pathResolve(dir) {
+//   return resolve(process.cwd(), '.', dir);
+// }
+// vite.config.js
+export default defineConfig({
+  css: {
+        preprocessorOptions: {
+         less: {
+         javascriptEnabled: true
+        }
+      }
+        },
+  server: {
+    host: '0.0.0.0',
+  },
+  plugins: [
+    createVuePlugin({
+      vueTemplateOptions: {},
+    }),
+  //  createSvgPlugin()
+  ],
+  resolve: {
+    extensions: ['.vue', '.mjs', '.js', 'less','.ts', '.jsx', '.tsx', '.json'],
+    alias: {
+      // vue2项目别名一般都是@，vue3中一般使用/@/, 为方便使用
+      '@': resolve('src'),
+    },
+  },
+});
